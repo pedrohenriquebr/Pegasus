cef0794812dcf77024a361ff8c29c11250df8f2d
PedroHenriqueBragaDaSilva <pedrohenriquebraga735@gmail.com>
2021-11-17 09:52:53 -0300

    update

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,8 +1,4 @@
 
 from api.main import app as application
-# from api.app import app
 
-app = application
-import os
-if __name__ == "__main__":
-        app.run(port=os.environ['PORT'])
+api = application
